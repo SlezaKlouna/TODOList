e850ff810289bed18cf32ecb505565c0967b510a
Samsara83 <moi.konferencii@gmail.com>
2020-01-29 21:26:51 +0200

    Правки

--- js/main.js
+++ js/main.js
@@ -1,62 +1,86 @@
 'use strict';
-
-let taskList = {
-    // idTask: taskID(),
-    taskContent: '',
-    taskState: ''
-}
+// const taskID = () => Math.random().toString(36).substr(2, 16);
 
 // Form
 const form = document.querySelector('.add-task');
 const formItem = document.querySelectorAll('.form-item');
 
-const addInput = document.querySelector('.add-input');
-const addBtn = document.querySelector('.addBtn');
+const addInput = document.querySelector("input[type='text']");
 
 // TodoList
 const taskBox = document.querySelector('.tasks');
+let counterId = 1;
 
-const createTodoList = task => {
-    let todoList = document.createElement('div');
-    let taskCheckbox = document.createElement('input');
-    let taskLabel = document.createElement('label');
+const todoApp = () => {
 
-    taskLabel.textContent = task.taskContent;
+    // const taskID = () => Math.random().toString(36).substr(2, 16);
 
-    let remove = document.createElement('a');
-    let restore = document.createElement('a');
+    let taskList = {
+        // id : taskID(),
+        taskContent: '',
+    }
 
-    // add class
-    todoList.className = 'task-box';
-    taskCheckbox.className = 'task-checkbox';
-    taskLabel.className = 'task-label';
+    // const storageQuery = () => {
+    //     let todo = taskBox.innerHTML;
+    //     localStorage.setItem('todo', todo);
+    // }
+    //
+    // if (localStorage.getItem('todo')){
+    //     taskBox.innerHTML = localStorage.getItem('todo');
+    // }
 
-    restore.innerHTML = '<a href="#" class="restore-task" data-state="">&#9998;</a>';
-    remove.innerHTML = ' <a href="#" class="remove-task" data-state="">&#10006;</a>';
+    const deleteTodo = element => {
+        element.addEventListener("click", event => {
+            event.preventDefault();
+            element.parentElement.remove();
+        });
+    }
 
-    //add atribute
-    taskCheckbox.type = 'checkbox';
-    taskLabel.type = 'label';
+    const createTodoList = task => {
+        let todoList = document.createElement('div');
+        let taskCheckbox = document.createElement('input');
+        let taskLabel = document.createElement('label');
 
-    //append element
-    todoList.append(taskCheckbox);
-    todoList.append(taskLabel);
-    todoList.append(restore);
-    todoList.append(remove);
+        taskLabel.textContent = task.taskContent;
 
-    taskBox.append(todoList);
+        let remove = document.createElement('a');
+        let edit = document.createElement('a');
 
-    console.log(taskBox);
+        // add class
+        todoList.className = 'task-box';
+        taskCheckbox.className = 'task-checkbox';
+        taskLabel.className = 'task-label';
+
+        edit.innerHTML = `<a href="#" class="edit-task" data-state="edit">&#9998;</a>`;
+        remove.innerHTML = `<a href="#" class="remove-task" data-state="remove">&#10006;</a>`;
+
+        //add atribute
+        taskCheckbox.type = 'checkbox';
+        taskLabel.type = 'label';
+
+        taskCheckbox.setAttribute('id', counterId);
+        taskLabel.setAttribute('for', counterId);
+
+        //append element
+        todoList.append(taskCheckbox, taskLabel, edit, remove);
+        addInput.value = "";
+        deleteTodo(remove);
+
+        taskBox.append(todoList);
+    }
+
+    formItem.forEach(item => {
+        item.addEventListener('input', event => {
+            taskList[event.target.name] = event.target.value;
+        });
+    });
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        createTodoList(taskList);
+        counterId++;
+    });
 }
 
-formItem.forEach(item => {
-    item.addEventListener('input', event => {
-        taskList[event.target.name] = event.target.value;
-    });
-})
-
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    createTodoList(taskList);
-});
+document.addEventListener('DOMContentLoaded', todoApp);
 
